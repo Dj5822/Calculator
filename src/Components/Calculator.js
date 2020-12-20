3f34c16ffbbc447538509d99366c3e2966ee053e
dj5822 <dj5822@gmail.com>
2020-12-21 12:25:36 +1300

    Added calculator component.

---
+++ src/Components/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  render(){
+    return (
+      <div>
+        <p>test</p>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
