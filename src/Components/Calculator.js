0c21bc7468bcee88dac88c73f6f83d3fb9abdfac
dj5822 <dj5822@gmail.com>
2020-12-21 14:13:17 +1300

    Added operator buttons.

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -5,7 +5,15 @@ class Calculator extends React.Component {
   render(){
     return (
       <div>
+        <div id="display"></div>
         <Numpad />
+        <button id="add">+</button>
+        <button id="subtrack">-</button>
+        <button id="multiple">*</button>
+        <button id="divide">/</button>
+        <button id="equals">=</button>
+        <button id="decimal">.</button>
+        <button id="clear">C</button>
       </div>
     );
   }
