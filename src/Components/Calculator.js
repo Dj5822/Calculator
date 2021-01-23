5b73e4b87801f92058062bffc7ea0c40b87d7e0c
dj5822 <dj5822@gmail.com>
2021-01-23 16:02:28 +1300

    Added display

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Display from './Display.js';
 import Keypad from './Keypad.js';
 
 class Calculator extends React.Component {
   render(){
     return (
       <div>
-        <div id="display"></div>
+        <Display id="display" />
         <Keypad />
       </div>
     );
--- src/Components/Keypad.js
+++ src/Components/Keypad.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import '../Style/style.css';
 
 class KeyPad extends React.Component {
   render(){
     return (
       <div id="keypad">
-        <button id="zero">0</button>
-        <button id="one">1</button>
-        <button id="two">2</button>
-        <button id="three">3</button>
-        <button id="four">4</button>
-        <button id="five">5</button>
-        <button id="six">6</button>
         <button id="seven">7</button>
         <button id="eight">8</button>
         <button id="nine">9</button>
-        <button id="add">+</button>
-        <button id="subtrack">-</button>
         <button id="multiple">*</button>
+        <button id="four">4</button>
+        <button id="five">5</button>
+        <button id="six">6</button>
+        <button id="subtrack">-</button>
+        <button id="one">1</button>
+        <button id="two">2</button>
+        <button id="three">3</button>
+        <button id="add">+</button>
+        <button id="zero">0</button>
         <button id="divide">/</button>
-        <button id="equals">=</button>
         <button id="decimal">.</button>
+        <button id="equals">=</button>
         <button id="clear">C</button>
       </div>
     );
